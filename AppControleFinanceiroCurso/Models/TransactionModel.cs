5960c149187765f783908ae190557518c41e392e
AlarconVinicius <alarcon.pqd74@gmail.com>
2023-05-20 13:16:58 -0300

    Criando TransactionModel e TransactionType

---
+++ AppControleFinanceiroCurso/Models/TransactionModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppControleFinanceiroCurso.Models.Enums;
+
+namespace AppControleFinanceiroCurso.Models
+{
+    public class TransactionModel
+    {
+        public int Id { get; set; }
+        public TransactionType Type { get; set; }
+        public String Name { get; set; }
+        public DateTimeOffset Date { get; set; }
+        public decimal Value { get; set; }
+    }
+}
