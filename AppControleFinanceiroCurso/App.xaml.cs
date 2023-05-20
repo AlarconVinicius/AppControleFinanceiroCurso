4c7cb5d2feebd1284ae15780200140f4a294320f
AlarconVinicius <alarcon.pqd74@gmail.com>
2023-05-20 10:59:31 -0300

    first commit

---
+++ AppControleFinanceiroCurso/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace AppControleFinanceiroCurso;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppControleFinanceiroCurso/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AppControleFinanceiroCurso;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
