286d0ef7e7605f5097801e12017794c150547f84
AlarconVinicius <alarcon.pqd74@gmail.com>
2023-05-20 13:56:36 -0300

    Limpando a Classe e o Enum

--- AppControleFinanceiroCurso/Models/Enums/TransactionType.cs
+++ AppControleFinanceiroCurso/Models/Enums/TransactionType.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AppControleFinanceiroCurso.Models.Enums
+﻿namespace AppControleFinanceiroCurso.Models.Enums
 {
     public enum TransactionType
     {
--- AppControleFinanceiroCurso/Models/TransactionModel.cs
+++ AppControleFinanceiroCurso/Models/TransactionModel.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using AppControleFinanceiroCurso.Models.Enums;
+﻿using AppControleFinanceiroCurso.Models.Enums;
+using LiteDB;
 
 namespace AppControleFinanceiroCurso.Models
 {
     public class TransactionModel
     {
+        [BsonId]
         public int Id { get; set; }
         public TransactionType Type { get; set; }
         public String Name { get; set; }
