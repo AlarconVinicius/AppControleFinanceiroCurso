d2b0c9c6e73f685ec6e012b75f78d8d3a0f54925
AlarconVinicius <alarcon.pqd74@gmail.com>
2023-05-20 14:09:14 -0300

    Adicionando Interface e Repositorio Transaction

---
+++ AppControleFinanceiroCurso/Interfaces/ITransactionRepositoy.cs
@@ -0,0 +1,12 @@
+﻿using AppControleFinanceiroCurso.Models;
+
+namespace AppControleFinanceiroCurso.Interfaces
+{
+    public interface ITransactionRepositoy
+    {
+        void Add(TransactionModel objeto);
+        void Delete(TransactionModel objeto);
+        List<TransactionModel> GetAll();
+        void Update(TransactionModel objeto);
+    }
+}
---
+++ AppControleFinanceiroCurso/Repositories/TransactionRepositoy.cs
@@ -0,0 +1,38 @@
+﻿using AppControleFinanceiroCurso.Interfaces;
+using AppControleFinanceiroCurso.Models;
+using LiteDB;
+
+namespace AppControleFinanceiroCurso.Repositories
+{
+    public class TransactionRepositoy : ITransactionRepositoy
+    {
+        private readonly LiteDatabase _database;
+        private readonly string _collectionName = "transactions";
+        public TransactionRepositoy()
+        {
+            _database = new LiteDatabase("Filename=C:/users/AppData/controle_financeiro_db.db;Connection=Shared");
+        }
+
+        public List<TransactionModel> GetAll()
+        {
+            return _database.GetCollection<TransactionModel>(_collectionName).Query().OrderByDescending(t => t.Date).ToList();
+        }
+
+        public void Add(TransactionModel objeto)
+        {
+           var coll =  _database.GetCollection<TransactionModel>(_collectionName);
+           coll.Insert(objeto);
+            coll.EnsureIndex(t => t.Date);
+        }
+        public void Update(TransactionModel objeto)
+        {
+            var coll = _database.GetCollection<TransactionModel>(_collectionName);
+            coll.Update(objeto);
+        }
+        public void Delete(TransactionModel objeto)
+        {
+            var coll = _database.GetCollection<TransactionModel>(_collectionName);
+            coll.Delete(objeto.Id);
+        }
+    }
+}
